e3b5809bb31a3a2bb283fbe006ba4aa505880181
Krisztina Pap <krisztina.pap@outlook.com>
2020-09-14 11:59:55 -0600

    Adds store, store.subscribe, and store.dispatch for own testing purposes;

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { createStore } from 'redux';
 import toDosReducer from './reducers/todos';
+import { addNewToDo } from './actions/todos';
 
 const store = createStore( toDosReducer );
 
+store.subscribe( () => console.log( store.getState() ) );
+
+store.dispatch( addNewToDo( "Research Redux" ) );
+store.dispatch( addNewToDo( "Eat lunch" ) );
 
 ReactDOM.render(
   <React.StrictMode>

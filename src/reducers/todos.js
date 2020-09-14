4ead0d343560bec5273a710ff181f65d17e74607
Krisztina Pap <krisztina.pap@outlook.com>
2020-09-14 11:37:12 -0600

    Adds toDosReducer;

---
+++ src/reducers/todos.js
@@ -0,0 +1,15 @@
+const toDosReducer = ( state =[], action ) => {
+    switch ( action.type ) {
+        case 'ADD_NEW_TO_DO':
+            const newTask = {
+                task: action.payload
+            }
+            const newToDoList = [...state];
+            newToDoList.push( newTask );
+            return newToDoList;
+        default:
+            return state;
+    }
+}
+
+export default toDosReducer;

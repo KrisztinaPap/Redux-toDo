eb401d7cd917a822cbc4c52897f575cb9e030228
Krisztina Pap <krisztina.pap@outlook.com>
2020-09-14 11:45:52 -0600

    Adds redux store to index.js;

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { createStore } from 'redux';
+import toDosReducer from './reducers/todos';
+
+const store = createStore( toDosReducer );
 
 
 ReactDOM.render(

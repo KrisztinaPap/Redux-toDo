2842329cff1dd5eb7364f098b024ad62bc49f052
Krisztina Pap <krisztina.pap@outlook.com>
2020-09-14 13:27:27 -0600

    Adds toDos component and initializes Redux store;

--- src/index.js
+++ src/index.js
@@ -5,8 +5,12 @@ import { createStore } from 'redux';
 import toDosReducer from './reducers/todos';
 import { addNewToDo } from './actions/todos';
 import { Provider } from 'react-redux';
+import ToDos from './components/ToDos';
 
-const store = createStore( toDosReducer );
+const store = createStore(
+  toDosReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 store.subscribe( () => console.log( store.getState() ) );
 
@@ -15,7 +19,9 @@ store.dispatch( addNewToDo( "Eat lunch" ) );
 
 ReactDOM.render(
   <Provider store={ store }>
-    Hey
+    <>
+      <ToDos />
+    </>
   </Provider>,
   document.getElementById('root')
 );
